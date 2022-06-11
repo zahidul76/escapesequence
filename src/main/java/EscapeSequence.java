46e6f63fdc8a02e512480adede9f260487f5f6de
zahidul76 <zahidul.islam20152015@gmail.com>
2022-06-11 15:35:56 -0500

    Initial Commit

---
+++ src/main/java/EscapeSequence.java
@@ -0,0 +1,48 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("123 test drive");
+        System.out.println("Irving Tx");
+
+        //OR
+
+
+        // Excape Sequence
+        // \n = new line
+        // \" = "
+        //  \\ = \     black slash
+        // \t = print tab in console
+        //' = print'
+        // \b = n/a
+
+        // req: Print tab in console
+        System.out.println("name \t\tphone");
+
+
+        System.out.println("123 test drive \nirving texas");
+
+        // print this in console
+        // book name is " lord of the rings"
+        // \" is used to print " in console
+        System.out.println("Book Name is \"Lord of the Rings\"");
+
+
+        //Quiz:
+        //  123\1 test drive
+
+        System.out.println("123\\1 test drive");
+
+
+
+//		print : name is 'Karim'
+        System.out.println("Name is 'Karim'");
+
+
+
+        // quiz:
+        // 123 test driver apt "23\2"
+
+        System.out.println("apt \"23\\2\"");
+
+
+    }
+}
